8e447dd96577944db0b5fe58a6bd723e820ab1fe
richiwu <richard_wu94@hotmail.com>
2018-09-15 22:45:15 -0400

    css adds

---
+++ src/features/event/EventDetailed/EventDetailedChat.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const EventDetailedChat = () => {
+  return (
+    <div>
+      <h1>chat</h1>
+    </div>
+  )
+}
+
+export default EventDetailedChat
--- src/features/event/EventDetailed/EventDetailedPage.jsx
+++ src/features/event/EventDetailed/EventDetailedPage.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
+import { Grid } from 'semantic-ui-react'
+import EventDetailedChat from './EventDetailedChat'  
+import EventDetailedHeader from './EventDetailedHeader'  
+import EventDetailedInfo from './EventDetailedInfo'  
+import EventDetailedSidebar from './EventDetailedSidebar'  
 
 const EventDetailedPage = () => {
   return (
     <div>
-      <h1>Event Detailed Page</h1>
+    <h1>hello</h1>
+    <Grid>
+        <Grid.Column width={10}>
+          <EventDetailedHeader />
+          <EventDetailedInfo />
+          <EventDetailedChat />
+        </Grid.Column>
+        <Grid.Column width={6}>
+          <EventDetailedSidebar />
+        </Grid.Column>
+    </Grid>
     </div>
   )
 }
